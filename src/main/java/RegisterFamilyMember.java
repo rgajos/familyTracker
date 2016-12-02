8da9e7ed738cbd073fa7c055fea9962a92186e06
Radek Gajos <rgajos@siseth.com>
2016-12-02 14:23:46 +0100

    ok

--- src/main/java/RegisterFamilyMember.java
+++ src/main/java/RegisterFamilyMember.java
@@ -49,6 +49,8 @@ public class RegisterFamilyMember extends HttpServlet {
         PrintWriter out = response.getWriter();
         Connection connection = null;
         PreparedStatement ps = null;
+        
+        int cnt =0;
         try {
             BufferedReader bufferedReader = request.getReader();
             JSONObject jsonObject = (JSONObject) JSONValue.parse(bufferedReader);
@@ -57,36 +59,37 @@ public class RegisterFamilyMember extends HttpServlet {
             Context initialContext = (Context) ic.lookup("java:comp/env");
             DataSource datasource = (DataSource) initialContext.lookup("jdbc/MySQLDS");
             connection = datasource.getConnection();
-
+            cnt++;
             String checkAddPeopleQuery = "select * from add_people where code='" + jsonObject.get("code").toString() + "'";
             ps = connection.prepareStatement(checkAddPeopleQuery);
             ResultSet rs = ps.executeQuery();
-
+            cnt++;
             int context = 0;
             long userId = 0L;
             long peopleId = 0L;
             int familyChange = 0;
-
+            cnt++;
             if (rs.next()) {
                 context = rs.getInt(3);
-                        
+                     cnt++;   
                 JSONObject json = new JSONObject();
                 JSONObject jsonSettings = new JSONObject();
                 JSONObject jsonMessages = new JSONObject();
                 JSONArray jSONArrayPeoples = new JSONArray();
                 JSONArray jSONArrayPlaces = new JSONArray();
                 JSONArray jSONArrayPlace2People = new JSONArray();
-
+                cnt++;
                 String getSettingsQuery = "select * from settings where ID='" + rs.getInt(4) + "'";
                 ps = connection.prepareStatement(getSettingsQuery);
                 rs = ps.executeQuery();
-
+                cnt++;
                 if (rs.next()) {
-                    
+                    cnt++;
                     userId = rs.getLong(6);
+                    cnt++;
                     familyChange = rs.getInt(2);
                     familyChange++;
-                    
+                    cnt++;
                     jsonSettings.put("id", rs.getLong(1));
                     jsonSettings.put("familyChange", familyChange);
                     jsonSettings.put("placesChange", rs.getInt(3));
@@ -94,24 +97,25 @@ public class RegisterFamilyMember extends HttpServlet {
                     jsonSettings.put("notifications", rs.getString(5));
                     jsonSettings.put("userId", userId);
                 }
-                
+                cnt++;
                 String updateFamilyChangeQuery = "update settings set FAMILY_CHANGE='"+familyChange+"' where ID='"+rs.getLong(1)+"'";
                 ps = connection.prepareStatement(updateFamilyChangeQuery);
                 ps.executeUpdate();
-
+                cnt++;
                 String getMessagesQuery = "select * from messages where ID='" + rs.getInt(5) + "'";
                 ps = connection.prepareStatement(getMessagesQuery);
                 rs = ps.executeQuery();
-
+                cnt++;
                 if (rs.next()) {
                     jsonMessages.put("id", rs.getLong(1));
+                    cnt++;
                     jsonMessages.put("msg", rs.getString(2));
                 }
-                
+                cnt++;
                 String getPlacesQuery = "select * from places where USER_ID='" + userId + "'";
                 ps = connection.prepareStatement(getPlacesQuery);
                 rs = ps.executeQuery();
-
+                cnt++;
                 while (rs.next()) {
                     JSONObject places = new JSONObject();
                     places.put("id", rs.getLong(1));
@@ -122,11 +126,11 @@ public class RegisterFamilyMember extends HttpServlet {
 
                     jSONArrayPlaces.add(places);
                 }
-
+                cnt++;
                 String getPlaces2PeopleQuery = "select * from place2people where USER_ID='" + userId + "'";
                 ps = connection.prepareStatement(getPlaces2PeopleQuery);
                 rs = ps.executeQuery();
-
+                cnt++;
                 while (rs.next()) {
                     JSONObject place2People = new JSONObject();
                     place2People.put("id", rs.getLong(1));
@@ -135,17 +139,17 @@ public class RegisterFamilyMember extends HttpServlet {
 
                     jSONArrayPlace2People.add(place2People);
                 }
-                
+                cnt++;
                 String insertLocalizationQuery = "insert into localizations values ()";
                 ps = connection.prepareStatement(insertLocalizationQuery, Statement.RETURN_GENERATED_KEYS);
                 ps.executeUpdate();
-
+                cnt++;
                 ResultSet generatedKeys = ps.getGeneratedKeys();
                 long localizationId = 0;
                 if (generatedKeys.next()) {
                     localizationId = generatedKeys.getInt(1);
                 }
-                
+                cnt++;
                 String insertPeopleQuery = "insert into people (name, user_id, localization_id, context) values (?,?,?,?)";
                 ps = connection.prepareStatement(insertPeopleQuery, Statement.RETURN_GENERATED_KEYS);
                 ps.setString(1, jsonObject.get("name").toString());
@@ -153,23 +157,23 @@ public class RegisterFamilyMember extends HttpServlet {
                 ps.setLong(3, localizationId);
                 ps.setInt(4, context);
                 ps.executeUpdate();
-
+                cnt++;
                 generatedKeys = ps.getGeneratedKeys();
                 peopleId = 0;
                 if (generatedKeys.next()) {
                     peopleId = generatedKeys.getInt(1);
                 }
-                
+                cnt++;
                 String updateLocalizationQuery = "update localizations set "
                         + "PEOPLE_ID='" + peopleId + "' where ID=" + localizationId + "";
                 ps = connection.prepareStatement(updateLocalizationQuery);
                 ps.executeUpdate();
-                
+                cnt++;
                 if (context == 1) {
                     String getPeopleQuery = "select * from people where USER_ID='" + userId + "'";
                     ps = connection.prepareStatement(getPeopleQuery);
                     rs = ps.executeQuery();
-
+                    cnt++;
                     while (rs.next()) {
                         JSONObject people = new JSONObject();
                         people.put("id", rs.getLong(1));
@@ -181,14 +185,14 @@ public class RegisterFamilyMember extends HttpServlet {
                         people.put("authorizedSpeed", rs.getInt(9));
                         jSONArrayPeoples.add(people);
                     }
-
+                    cnt++;
                     json.put("peoples", jSONArrayPeoples);
                 }
                 
                 String deleteAddPeopleQuery = "delete from add_people where code=" + jsonObject.get("code").toString();
                 ps = connection.prepareStatement(deleteAddPeopleQuery);
                 ps.executeUpdate();
-                
+                cnt++;
                 json.put("settings", jsonSettings);
                 json.put("messages", jsonMessages);
                 json.put("places", jSONArrayPlaces);
@@ -207,17 +211,17 @@ public class RegisterFamilyMember extends HttpServlet {
         } catch (IOException ex) {
             JSONObject json = new JSONObject();
             json.put("error", 2);
-            json.put("desc", ex.getMessage());
+            json.put("desc", ex.getMessage()+ cnt);
             response.getWriter().write(json.toString());
         } catch (NamingException ex) {
             JSONObject json = new JSONObject();
             json.put("error", 2);
-            json.put("desc", ex.getMessage());
+            json.put("desc", ex.getMessage()+ cnt);
             response.getWriter().write(json.toString());
         } catch (SQLException ex) {
             JSONObject json = new JSONObject();
             json.put("error", 2);
-            json.put("desc", ex.getMessage());
+            json.put("desc", ex.getMessage()+cnt);
             response.getWriter().write(json.toString());
         } finally {
             try {
